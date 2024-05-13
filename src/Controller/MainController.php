d1ad2a21c2d22deca82d82df2bd7c180c971f1fb
Stefan <stefan@studiokhi.com>
2024-05-13 15:57:07 +0200

    feat(templates): create all pages templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,4 +14,34 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig');
     }
+
+    #[Route('/l-association', name: 'asso')]
+    public function asso(): Response
+    {
+        return $this->render('main/asso.html.twig');
+    }
+
+    #[Route('/le-topo', name: 'topo')]
+    public function topo(): Response
+    {
+        return $this->render('main/topo.html.twig');
+    }
+
+    #[Route('/le-festival', name: 'festival')]
+    public function festival(): Response
+    {
+        return $this->render('main/festival.html.twig');
+    }
+
+    #[Route('/le-shop', name: 'shop')]
+    public function shop(): Response
+    {
+        return $this->render('main/shop.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig');
+    }
 }
