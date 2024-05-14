a30fdc33973d95874972be7b7e7135c03432e848
Stefan <stefan@studiokhi.com>
2024-05-14 15:22:33 +0200

    fix(menu mobile): remove scroll body

--- assets/app.js
+++ assets/app.js
@@ -4,13 +4,16 @@ import './styles/app.css';
 const iconHeader = document.querySelector('.header__icon')
 const menuHeader = document.querySelector('.header__menu')
 const closeHeader = document.querySelector('.header__close')
+const body = document.querySelector('body')
 
 iconHeader.addEventListener('click', () => {
     menuHeader.style.transform = 'translate(0,0)'
+    body.style.overflow = 'hidden'
 })
 
 closeHeader.addEventListener('click', () => {
     menuHeader.style.transform = 'translate(-120%,0)'
+    body.style.overflow = 'auto'
 })
 
 // Header background scroll
